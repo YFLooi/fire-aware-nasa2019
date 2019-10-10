f1b72ed8b87a6714c5030c2e55daf533bc0dc2e3
YF Looi <40136494+YFLooi@users.noreply.github.com>
2019-10-11 06:56:01 +0800

    moved js and css files into same folder as index.html to work with Express, removed '&callback' on Maps API link which is bugging the console about an 'empty promise'

--- server.js
+++ server.js
@@ -2,10 +2,22 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '/dist/index.html'));
+//__dirname returns the directory that the currently executing script is in
+//Thus, the resulting path is: ./SpaceApps_vanillaHTML/dist/index.html
+//Ref: https://stackoverflow.com/questions/25463423/res-sendfile-absolute-path
+app.use(express.static(__dirname+'/dist'));
+
+app.get('/', function(req,res){
+	console.log('Main page loading...');
+	res.render('./dist/index.html');
 });
 
+/** 
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, './dist','index.html'));
+});
+*/
+
 app.listen(process.env.PORT || 4000, function () {
-    console.log('Node app is working!');
+    console.log('App running on port 8080 or 4000');
 });
